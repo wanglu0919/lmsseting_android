136811e1837baacac4982e7e3cad7e4fbeafa693
wanglu0919 <wanglu0919@163.com>
2013-12-10 22:11:10 +0800

    网络访问框架搭建

--- lmsssetting/src/com/challentec/lmssseting/app/MainActivity.java
+++ lmsssetting/src/com/challentec/lmssseting/app/MainActivity.java
@@ -3,19 +3,24 @@ package com.challentec.lmssseting.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.challentec.lmssseting.ui.SettingActivity;
 
 public class MainActivity extends Activity {
 	private Button main_btn_connect;
-
+	private long lastExitTime = 0;
+	private AppManager appManager;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_layout);
+		appManager=AppManager.getManager(this);
+		appManager.addActivity(this);
 		findViews();
 		addListeners();
 	}
@@ -39,4 +44,32 @@ public class MainActivity extends Activity {
 
 	}
 
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			exit();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+	
+	/**
+	 * 退出app
+	 * 
+	 * @author 泰得利通 wanglu
+	 */
+
+	public void exit() {
+
+		if ((System.currentTimeMillis() - lastExitTime) > 2000) {
+			Toast.makeText(getApplicationContext(),
+					"再按一次退出创联物联网测试程序",
+					Toast.LENGTH_SHORT).show();
+			lastExitTime = System.currentTimeMillis();
+		} else {
+
+			AppManager.getManager(this).appExit(this);
+		}
+	}
 }
--- lmsssetting/src/com/challentec/lmssseting/ui/StateQuearyActivity.java
+++ lmsssetting/src/com/challentec/lmssseting/ui/StateQuearyActivity.java
@@ -1,20 +1,55 @@
 package com.challentec.lmssseting.ui;
 
-import android.view.View;
+import java.util.ArrayList;
+import java.util.List;
 
+import android.view.View;
+import android.widget.ListView;
+
+import com.challentec.lmsseting.bean.StateParamsItem;
+import com.challentec.lmssseting.adapter.ParamsItemAdapter;
 import com.challentec.lmssseting.app.R;
 
 /**
- * 状态查询 
+ * 状态查询
+ * 
  * @author wanglu 泰得利通
- *
+ * 
  */
 public class StateQuearyActivity extends BaseActivity {
 
-	
+	private ListView params_lv;
+	private List<StateParamsItem> stateParamsItems;
+	private ParamsItemAdapter paramsItemAdapter;
+	private String itemName[] = new String[] { "安全回路", "厅门锁", "上强迫减速", "下强迫减速",
+			"机房检修", "轿门锁", "锁梯信号", "轿顶检修", "上门区", "下门区", "运行", "消防", "上慢车限位",
+			"下慢车限位", "抱闸", "警铃", "平层", "上行", "下行" };
+
+	private String itemValue[] = new String[] { "正常", "厅门关闭", "不在顶层", "在底层",
+			"检修状态", "轿门打开", "锁梯", "检修状态", "出上门区", "出下门区", "电梯停止", "消防状态",
+			"在上限位", "在下限位", "抱闸松开", "警铃触发", "不在平层", "上行", "下行" };
+
 	@Override
 	protected void initMainView(View mainView) {
-		
+		params_lv = (ListView) mainView.findViewById(R.id.params_lv);
+
+		initData();
+	}
+
+	private void initData() {
+		stateParamsItems = new ArrayList<StateParamsItem>();
+
+		for (int i = 0; i < itemName.length; i++) {
+			StateParamsItem stateParamsItem = new StateParamsItem();
+			stateParamsItem.setItemName(itemName[i]);
+			stateParamsItem.setItemValue(itemValue[i]);
+			stateParamsItems.add(stateParamsItem);
+
+		}
+
+		paramsItemAdapter = new ParamsItemAdapter(stateParamsItems, this);
+		params_lv.setAdapter(paramsItemAdapter);
+
 	}
 
 	@Override
